67dd06adcd6cc84b3df1cf51daae267f28e02d83
eldhoral <eldhoral@gmail.com>
2022-05-18 16:05:16 +0700

    change directory of env file to ./params/

---
+++ app/migrations/sql/0010-create_tbl_tnc_subtitle.sql
@@ -0,0 +1,14 @@
+-- +migrate Up
+CREATE TABLE `tbl_tnc_subtitle` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_tnc_title` int NOT NULL,
+  `subtitle` varchar(100) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `tbl_tnc_subtitle_FK` (`id_tnc_title`),
+  CONSTRAINT `tbl_tnc_subtitle_FK` FOREIGN KEY (`id_tnc_title`) REFERENCES `tbl_tnc_title` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+-- +migrate Down
+DROP TABLE tbl_tnc_subtitle;
